68afdaa2c5de5c800986d6ab159bbd9903ace13f
Charlie Jindra <39534075+charliejindra@users.noreply.github.com>
2020-06-22 15:42:32 -0400

    Add project files.

---
+++ sampleProj/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace sampleProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hey! What's your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("What's your age?");
+            string strAge = Console.ReadLine();
+            int age = Int32.Parse(strAge);
+
+            Console.WriteLine("What's your favorite color?");
+            string color = Console.ReadLine();
+
+            ConsoleColor bgColor = (ConsoleColor) Enum.Parse(typeof(ConsoleColor), color);
+            Console.BackgroundColor = bgColor;
+            Console.Clear();
+
+            Console.WriteLine("Hi " + name + "!");
+
+            if (age > 20)
+            {
+                Console.WriteLine("Have a beer!");
+            }
+            else
+            {
+                Console.WriteLine("Sorry, rootbeers only.");
+            }
+
+            
+
+        }
+    }
+}
+
